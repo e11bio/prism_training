1e6d95614a32f906fa964d42c4deccecb161ccec
pattonw <pattonw@hhmi.org>
2025-09-25 20:00:53 -0700

    add uniform training

--- prism_training/train/losses.py
+++ prism_training/train/losses.py
@@ -1,5 +1,196 @@
 import torch
 
+from itertools import combinations
+
+import torch
+
+import wandb
+
+import time
+
+
+class ContrastiveLoss(torch.nn.Module):
+    def __init__(
+        self,
+        t: int = 1,
+        a: int = 1,
+        regularize: bool = False,
+        background_relabel: bool = False,
+        dynamic_balancing: bool = False,
+        uniform_emb: bool = False,
+        batchwise: bool = True,
+        prefix: str = "",
+    ):
+        super().__init__()
+
+        # todo - add docstrings
+
+        self.t = t
+        self.a = a
+        self.regularize = regularize
+        self.background_relabel = background_relabel
+        self.dynamic_balancing = dynamic_balancing
+        self.uniform_emb = uniform_emb
+        self.batchwise = batchwise
+        self.prefix = prefix
+
+    def forward(self, prediction, gt):
+        """
+        given ground truth and prediction, compute loss that minimizes
+        the variance of the prediction for each label in the ground truth,
+        and maximizes the distance between the center_means of the prediction within
+        each ground truth label
+        """
+        t1 = time.time()
+        b, c, z, y, x = prediction.shape
+        gt = gt.view(b, z, y, x)
+        if self.background_relabel:
+            gt += 1
+
+        if not self.batchwise:
+            intra_similarities = []
+            inter_similarities = []
+            for sample in range(b):
+                (
+                    intra_similarities_sample,
+                    inter_similarities_sample,
+                ) = self.compute_similarities(prediction[sample], gt[sample])
+                intra_similarities.append(intra_similarities_sample)
+                inter_similarities.append(inter_similarities_sample)
+            intra_similarities = torch.cat(intra_similarities, dim=0)
+            inter_similarities = torch.cat(inter_similarities, dim=0)
+        else:
+            intra_similarities, inter_similarities = self.compute_similarities(
+                prediction.transpose(0, 1), gt
+            )
+
+        intra_similarity = torch.mean(intra_similarities)
+
+        # can we get rid of this line?
+        # intra_loss = torch.mean((1 - intra_similarities) ** 2)
+
+        intra_loss = -intra_similarity
+
+        if self.regularize:
+            normed_pred = torch.nn.functional.normalize(prediction, p=2, dim=1)
+            reg_loss = (
+                torch.nn.MSELoss()(prediction, normed_pred) if self.regularize else None
+            )
+
+        if len(inter_similarities) > 0:
+            inter_similarity = torch.mean(inter_similarities)
+            clipped_inter_similarity = torch.mean(torch.clamp(inter_similarities, 0, 1))
+
+            gaussian_potential_similarity = (
+                torch.log(
+                    torch.mean(torch.exp(2 * self.t * inter_similarities - 2 * self.t))
+                )
+                / (2 * self.t)
+            ) + 1
+
+            inter_loss = (
+                gaussian_potential_similarity
+                if self.uniform_emb
+                else clipped_inter_similarity
+            )
+            balance_term = 1 - inter_loss if self.dynamic_balancing else 1
+
+            try:
+                wandb.log(
+                    {
+                        f"{self.prefix}inter_similarity": inter_similarity,
+                        f"{self.prefix}clipped_inter_similarity": clipped_inter_similarity,
+                        f"{self.prefix}gaussian_potential_similarity": gaussian_potential_similarity,
+                        f"{self.prefix}balance_term": (
+                            balance_term if self.dynamic_balancing else None
+                        ),
+                    },
+                    commit=False,
+                )
+            except wandb.errors.Error as e:
+                print(
+                    e,
+                    (
+                        f"{self.prefix}inter_similarity {inter_similarity.item():.3f}\n"
+                        f"{self.prefix}clipped_inter_similarity "
+                        f"{clipped_inter_similarity.item():.3f}\n"
+                        f"{self.prefix}gaussian_potential_similarity "
+                        f"{gaussian_potential_similarity.item():.3f}"
+                    ),
+                )
+        else:
+            balance_term = torch.tensor(1, device=prediction.device)
+            inter_loss = torch.tensor(0, device=prediction.device)
+
+        try:
+            wandb.log(
+                {
+                    f"{self.prefix}intra_similarity": intra_similarity,
+                    f"{self.prefix}reg_loss": reg_loss if self.regularize else None,
+                },
+                commit=False,
+            )
+        except wandb.errors.Error as e:
+            print(
+                e,
+                (
+                    f"{self.prefix}intra_similarity {intra_similarity.item():.3f}\n"
+                    f"{self.prefix}reg_loss "
+                    f"{(reg_loss.item() if self.regularize else 0.0):.3f}\n"
+                ),
+            )
+
+        print(f"Contrastive loss computed in {time.time() - t1:.3f} seconds")
+
+        return (
+            inter_loss
+            + balance_term * intra_loss
+            + (reg_loss if self.regularize else 0)
+        )
+
+    def compute_similarities(self, prediction, gt):
+        # Compute the variance of the prediction for each label in the gt
+        c, *_ = prediction.shape
+        intra_similarities = []
+        unique_labels = list(torch.unique(gt))
+        center_means = []
+        for label in unique_labels:
+            if label == 0:
+                continue
+            mean_vec = []
+            mask = gt == label
+            masked_preds = []
+            for i in range(c):
+                masked_pred = prediction[i][mask]
+                masked_preds.append(masked_pred)
+
+                mean = torch.mean(masked_pred)
+                mean_vec.append(mean)
+            masked_pred = torch.stack(masked_preds)
+            mean_vec = torch.stack(mean_vec)
+
+            if label > 0 and len(masked_pred) > 1:
+                dot_prod = torch.nn.CosineSimilarity(dim=0)(
+                    masked_pred, mean_vec.view(c, 1)
+                )
+                intra_similarities.append(torch.mean(dot_prod))
+
+            center_means.append(mean_vec)
+
+        intra_similarities = torch.stack(intra_similarities)
+
+        # Compute the distance between the center_means of the prediction within
+        # each ground truth label
+        if len(center_means) < 2:
+            return intra_similarities, torch.empty(0, device=prediction.device)
+        inter_similarities = []
+        for center_a, center_b in combinations(center_means, 2):
+            similarity = torch.nn.CosineSimilarity(dim=0)(center_a, center_b)
+            inter_similarities.append(similarity)
+        inter_similarities = torch.stack(inter_similarities)
+
+        return intra_similarities, inter_similarities
+
 
 class WeightedBCELossMultiChannel(torch.nn.BCELoss):
     def __init__(self, reduction="mean"):
--- prism_training/train/uniform/train.py
+++ prism_training/train/uniform/train.py
@@ -9,18 +9,14 @@ dask.config.set(scheduler="single-threaded")
 
 from funlib.persistence import open_ds, Array
 from prism_training.train.models import ChannelAgnosticWrapper, SigmoidWrapper, UNet
+from prism_training.train.losses import ContrastiveLoss
 
 from pathlib import Path
 
-from dacapo_toolbox.dataset import (
-    iterable_dataset,
-    DeformAugmentConfig,
-    SimpleAugmentConfig,
-)
+from dacapo_toolbox.dataset import iterable_dataset
 from dacapo_toolbox.vis.preview import cube
 
 import torch
-import torchvision
 
 # Training parameters:
 NUM_ITERATIONS = 1
@@ -46,89 +42,68 @@ def to_array(data, metadata):
     )
 
 
-fmaps = 12
+fmaps = 1024
 # Create the model:
 unet = UNet(
-    in_channels=1,
+    in_channels=18,
     num_fmaps=fmaps,
     fmap_inc_factor=2,
-    downsample_factors=[
-        (1, 2, 2),
-        (2, 2, 2),
-        (2, 2, 2),
-    ],
+    downsample_factors=[],
     kernel_size_down=[
-        [(1, 3, 3), (1, 3, 3)],
-        [(1, 3, 3), (1, 3, 3)],
-        [(3, 3, 3), (3, 3, 3)],
-        [(3, 3, 3), (3, 3, 3)],
-    ],
-    kernel_size_up=[
-        [(1, 3, 3), (1, 3, 3)],
-        [(1, 3, 3), (1, 3, 3)],
-        [(3, 3, 3), (3, 3, 3)],
+        [
+            (1, 1, 1),
+            (1, 1, 1),
+            (1, 1, 1),
+            (1, 1, 1),
+            (1, 1, 1),
+            (1, 1, 1),
+        ],
     ],
 )
 
-model = SigmoidWrapper(
-    ChannelAgnosticWrapper(
-        torch.nn.Sequential(
-            unet,
-            torch.nn.Conv3d(fmaps, 1, kernel_size=1),
-        )
-    )
+model = torch.nn.Sequential(
+    unet,
+    torch.nn.Conv3d(fmaps, 24, kernel_size=1),
 )
 
-if Path("model_checkpoint_100000").exists():
+if Path("model_checkpoint_10000").exists():
     print("Loading model checkpoint...")
     model.load_state_dict(
-        torch.load("model_checkpoint_100000", map_location=torch.device("cpu"))[
+        torch.load("model_checkpoint_10000", map_location=torch.device("cpu"))[
             "model_state_dict"
         ]
     )
 
 
-input_shape = (48, 148, 148)
-output_shape = (16, 56, 56)
+input_shape = (10, 212, 212)
+output_shape = (10, 212, 212)
 
 # load the data
-sample = Path("../../data/semantic/example_data.zarr")
+# sample = Path("../../data/instance/example_data.zarr")
+# raw = open_ds(sample / "enhanced")
+# labels = open_ds(sample / "labels")
+
 raw = open_ds(
     "/home/arlo/Desktop/Workspace/Projects/lonely_santa/data/training/lonely_santa/sparse/sample_6.zarr/enhanced_3d"
 )
-labels = open_ds(sample / "fgbg")
-mask = open_ds(sample / "border_mask")
+labels = open_ds(
+    "/home/arlo/Desktop/Workspace/Projects/lonely_santa/data/training/lonely_santa/sparse/sample_6.zarr/eroded_labels"
+)
+
 
 dataset = iterable_dataset(
     datasets={
         "raw": raw,
         "labels": labels,
-        "mask": mask,
     },
     shapes={
         "raw": input_shape,
         "labels": output_shape,
-        "mask": output_shape,
     },
     transforms={
         "raw": lambda x: x.float() / 255,
-        "labels": lambda x: x.float(),
-        "mask": lambda x: x.float(),
+        "labels": lambda x: x.long(),
     },
-    deform_augment_config=DeformAugmentConfig(
-        p=0.5,
-        control_point_spacing=(2, 10, 10),
-        jitter_sigma=(0.5, 2, 2),
-        rotate=True,
-        subsample=4,
-        rotation_axes=(1, 2),
-        scale_interval=(0.9, 1.1),
-    ),
-    simple_augment_config=SimpleAugmentConfig(
-        p=1.0,
-        mirror_only=(1, 2),
-        transpose_only=(1, 2),
-    ),
 )
 
 dataloader = torch.utils.data.DataLoader(
@@ -137,7 +112,15 @@ dataloader = torch.utils.data.DataLoader(
 )
 
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
-loss_func = torchvision.ops.sigmoid_focal_loss
+loss_func = ContrastiveLoss(
+    t=2,
+    a=1,
+    regularize=True,
+    background_relabel=False,
+    uniform_emb=True,
+    dynamic_balancing=False,
+    batchwise=True,
+)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = model.to(device)
@@ -145,15 +128,14 @@ for iteration, batch in enumerate(dataloader):
     if iteration == NUM_ITERATIONS:
         break
 
-    raw, labels, mask = (
+    raw, labels = (
         batch["raw"].to(device),
         batch["labels"].to(device),
-        batch["mask"].to(device),
     )
 
     optimizer.zero_grad()
     pred = model(raw)
-    loss_val = (loss_func(pred, labels) * mask).sum() / mask.sum()
+    loss_val = loss_func(pred, labels)
     loss_val.backward()
     optimizer.step()
 
@@ -166,7 +148,7 @@ for iteration, batch in enumerate(dataloader):
             labels.cpu().detach().numpy()[0], batch["metadata"]["labels"]
         )
         raw_arr.lazy_op(labels_arr.roi)
-        print(pred.min().item(), pred.max().item())
+        print(pred.shape, raw.shape, labels.shape)
         cube(
             arrays={
                 "Raw": raw_arr,
@@ -179,7 +161,7 @@ for iteration, batch in enumerate(dataloader):
             array_types={
                 "Raw": "pca",
                 "Labels": "labels",
-                "Pred": "raw",
+                "Pred": "pca",
             },
             filename=SNAPSHOT_DIR / f"{iteration}.jpg",
             title="Prediction",
